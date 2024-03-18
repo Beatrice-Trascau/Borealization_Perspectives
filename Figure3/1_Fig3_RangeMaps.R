8f8ac2bd8f5b32bb66e8e445d9d93862db51dd51
James D. M. Speed <speed@ntnu.no>
2024-03-18 15:41:37 +0100

    Starting Figure 3 with mammal range maps from IUCN

---
+++ Figure3/1_Fig3_RangeMaps.R
@@ -0,0 +1,80 @@
+#Figure3
+
+#Paired boreal and Arctic species
+#Red fox, arctic fox; Moose, muskox; salix lanata, Salix polaris; Vaccinium myrtillus, Cassiope tetragona
+
+#a Range maps, #b temperature niches, #c trait distributions
+
+if(!"remotes" %in% installed.packages()[,"Package"]) install.packages("remotes")
+
+# Install rasterSp from Github if not previously installed
+if(!"rasterSp" %in% installed.packages()[,"Package"]) remotes::install_github("RS-eco/rasterSp", build_vignettes = T)
+
+#Libraries
+library(sf)
+library(ggplot2)
+library(terra)
+library(tidyterra)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(tidyverse)
+library(remotes)
+library(rasterSP)
+
+# #Biome maps -------------------------------------------------------------
+
+
+#Load WWF biomes
+#Citation: Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.
+globalbiomes<-st_read("Figure3/data/BiomesWWF","wwf_terr_ecos")
+
+#Biome 6 is boreal forest and 11 is tundra
+borealforest<-st_union(globalbiomes[globalbiomes$BIOME==6,])
+tundra<-st_union(globalbiomes[globalbiomes$BIOME==11 &(globalbiomes$REALM=="PA"|globalbiomes$REALM=="NA"), ])
+borealtundra<-globalbiomes[(globalbiomes$BIOME==6 | globalbiomes$BIOME==11)& (globalbiomes$REALM=="PA"|globalbiomes$REALM=="NA"),]
+
+bandt<-st_intersection(borealforest,tundra)
+
+#World map
+world <- ne_countries(scale = "medium", returnclass = "sf")
+class(world)
+
+
+#Set polar projection
+projchoice<-"+proj=laea +lat_0=90 +lon_0=0 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs "
+borealtundra$BIOME<-as.factor(borealtundra$BIOME)
+
+mycols<-c("darkolivegreen","darkgoldenrod")
+
+ggplot()+geom_sf(data=borealtundra,aes(fill=BIOME),color=NA)+
+  geom_sf(data=world,fill=NA)+theme_bw()+theme(axis.text.x = element_blank(),axis.text.y = element_blank(),legend.position=c(0.2,0.9))+
+  coord_sf(crs = projchoice,ylim=c(-703086, 7071423),xlim=c(-505347.4, 8526158))+
+  scale_fill_manual(labels=c("Boreal forest","Arctic tundra"),"Biome",values=mycols)
+
+
+# Range data --------------------------------------------------------------
+#IUCN Range maps for vertebrates
+#Downloaded from IUCN - citation in the folder
+
+arcticfox<-st_read("Figure3/data/MammalRangeMaps/arcticfox","data_0")
+muskox<-st_read("Figure3/data/MammalRangeMaps/muskox","data_0")
+redfox<-st_read("Figure3/data/MammalRangeMaps/redfox","data_0")
+moose<-st_read("Figure3/data/MammalRangeMaps/moose","data_0")
+
+#Plot each pair, using alpha blending (probably can find some better colours)
+ggplot()+#geom_sf(data=borealtundra,fill=NA,color="black")+
+  geom_sf(data=world,fill=NA)+theme_bw()+theme(axis.text.x = element_blank(),axis.text.y = element_blank(),legend.position=c(0.2,0.9))+
+  geom_sf(data=moose,fill=mycols[1],color=NA,alpha=0.5)+geom_sf(data=muskox,fill=mycols[2],color=NA,alpha=0.5)+
+  coord_sf(crs = projchoice,ylim=c(-703086, 7071423),xlim=c(-505347.4, 8526158))+
+  scale_fill_manual(labels=c("Moose","Muskox"),"Species",values=mycols)
+
+ggplot()+#geom_sf(data=borealtundra,aes(fill=BIOME),color=NA)+
+  geom_sf(data=world,fill=NA)+theme_bw()+theme(axis.text.x = element_blank(),axis.text.y = element_blank(),legend.position=c(0.2,0.9))+
+  geom_sf(data=redfox,fill=mycols[1],color=NA,alpha=0.5)+geom_sf(data=arcticfox,fill=mycols[2],color=NA,alpha=0.5)+
+  coord_sf(crs = projchoice,ylim=c(-703086, 7071423),xlim=c(-505347.4, 8526158))
+
+
+
+
+#GBIF occurrence data for plants
+
